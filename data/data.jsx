6f0de21ae65baff6aae3cdb1873e5e9898006889
Senninseyi <seyi.oyebamiji@gmail.com>
2022-11-05 14:57:36 +0100

    chore: create data

---
+++ data/data.jsx
@@ -0,0 +1,49 @@
+import Schools from "../assets/schools.svg";
+
+export const schoolData = [
+  {
+    location: "Ojo",
+    logo: Schools,
+    name: "NNSS",
+  },
+  {
+    location: "Abeokuta",
+    logo: Schools,
+    name: "NNSS",
+  },
+  {
+    location: "Calabar",
+    logo: Schools,
+    name: "NNSS",
+  },
+  {
+    location: "Porthacort",
+    logo: Schools,
+    name: "NNSS",
+  },
+  {
+    location: "Ogbomosho",
+    logo: Schools,
+    name: "NNSS",
+  },
+  {
+    location: "Imeri",
+    logo: Schools,
+    name: "NNSS",
+  },
+  {
+    location: "Kuje",
+    logo: Schools,
+    name: "NNSS",
+  },
+  {
+    location: "Okura",
+    logo: Schools,
+    name: "NNSS",
+  },
+  {
+    location: "Ikot-Ntuen",
+    logo: Schools,
+    name: "NNMS",
+  },
+];
