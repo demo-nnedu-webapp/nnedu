0ec1a107652b96bc1089d68f50d06796a1c4300a
Senninseyi <seyi.oyebamiji@gmail.com>
2022-11-10 15:15:44 +0100

    chore: update on profile page

--- pages/dashboard/profile.jsx
+++ pages/dashboard/profile.jsx
@@ -1,8 +1,12 @@
+import { Form } from "antd";
 import Head from "next/head";
 import React from "react";
+import { CustomAvatar } from "../../components/customAvatar/customAvatar";
 import DashboardLayout from "../../components/layout/dashboardLayout";
+import { dummyprofiledata } from "../../data/data";
 
 function Profile() {
+  const date = new Date().toJSON().slice(0, 10);
   return (
     <>
       <div>
@@ -13,9 +17,176 @@ function Profile() {
         </Head>
       </div>
       <DashboardLayout>
-      <h2 className="font-bold text-2xl font-mono text-[#1f1839]">Profile</h2>
-        <div className="bg-[#1f1839] w-full p-4">
-          <div className="w-full flex justify-between"></div>
+        <div className="mt-4 w-full max-w-3xl">
+          <h2 className="font-semibold text-2xl font-inter tracking-[0.06em] text-primary">
+            Profile
+          </h2>
+          <div className="bg-white w-full mt-4 p-4 border-t-4 border-primary shadow-md">
+            <div className="p-4">
+              <div className="w-full grid lg:grid-cols-6 gap-5">
+                <div className="col-span-2 flex justify-start lg:justify-center">
+                  <div className="flex flex-col items-center gap-4 w-fit">
+                    <div className="bg-secondary w-fit py-1 px-1.5">
+                      <p className="font-inter font-semibold tracking-[0.06em]">
+                        ID/
+                        <span className="font-montserrat font-normal">
+                          NNSS1101
+                        </span>
+                      </p>
+                    </div>
+                    <div className="w-full flex justify-center">
+                      <Form>
+                        <CustomAvatar />
+                      </Form>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-span-4 grid grid-cols-2 lg:grid-cols-3 gap-4">
+                  <div className="flex flex-col gap-y-5 justify-between">
+                    <div className="flex flex-col">
+                      <h4 className="capitalize font-inter tracking-[0.06em] font-medium text-base">
+                        First name
+                      </h4>
+                      <p className="font-montserrat font-normal tracking-[0.06em] text-sm">
+                        Ciroma
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col">
+                      <h4 className="capitalize font-inter tracking-[0.06em] font-medium text-base">
+                        Last name
+                      </h4>
+                      <p className="font-montserrat font-normal tracking-[0.06em] text-sm">
+                        Abdullahi
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col">
+                      <h4 className="capitalize font-inter tracking-[0.06em] font-medium text-base">
+                        Maiden name
+                      </h4>
+                      <p className="font-montserrat font-normal tracking-[0.06em] text-sm">
+                        Adelaide
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-y-5 justify-between">
+                    <div className="flex flex-col">
+                      <h4 className="capitalize font-inter tracking-[0.06em] font-medium text-base">
+                        Date of Birth
+                      </h4>
+                      <p className="font-montserrat font-normal tracking-[0.06em] text-sm">
+                        {date}
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col">
+                      <h4 className="capitalize font-inter tracking-[0.06em] font-medium text-base">
+                        Nationality
+                      </h4>
+                      <p className="font-montserrat font-normal tracking-[0.06em] text-sm">
+                        Nigerian
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col">
+                      <h4 className="capitalize font-inter tracking-[0.06em] font-medium text-base">
+                        Religion
+                      </h4>
+                      <p className="font-montserrat font-normal tracking-[0.06em] text-sm">
+                        Unknown
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-y-5 justify-between">
+                    <div className="flex flex-col">
+                      <h4 className="capitalize font-inter tracking-[0.06em] font-medium text-base">
+                        Place of Birth
+                      </h4>
+                      <p className="font-montserrat font-normal tracking-[0.06em] text-sm">
+                        Unknown
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col">
+                      <h4 className="capitalize font-inter tracking-[0.06em] font-medium text-base">
+                        Gender
+                      </h4>
+                      <p className="font-montserrat font-normal tracking-[0.06em] text-sm">
+                        Unknown
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-12">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            <div>
+              <h2 className="font-semibold text-2xl font-inter tracking-[0.06em] text-primary">
+                Academic details
+              </h2>
+              <div className="bg-white shadow-md p-6 flex justify-center mt-4 border-t-4 border-primary">
+                <div className="grid grid-cols-2 justify-start items-center gap-y-5 gap-x-16 p-4">
+                  <div className="w-fit flex flex-col">
+                    <h4 className="font-inter font-medium text-base tracking-[0.06em]">
+                      School
+                    </h4>
+                    <p className="tracking-[0.06em] text-sm font-montserrat font-normal">
+                      NNSS OJO
+                    </p>
+                  </div>
+
+                  <div className="w-fit flex flex-col">
+                    <h4 className="font-inter font-medium text-base tracking-[0.06em]">
+                      Class
+                    </h4>
+                    <p className="tracking-[0.06em] text-sm font-montserrat font-normal">
+                      SS3
+                    </p>
+                  </div>
+
+                  <div className="w-fit flex flex-col">
+                    <h4 className="font-inter font-medium text-base tracking-[0.06em]">
+                      Admission date
+                    </h4>
+                    <p className="tracking-[0.06em] text-sm font-montserrat font-normal">
+                      Unknown
+                    </p>
+                  </div>
+
+                  <div className="w-fit flex flex-col">
+                    <h4 className="font-inter font-medium text-base tracking-[0.06em]">
+                      Arm
+                    </h4>
+                    <p className="tracking-[0.06em] text-sm font-montserrat font-normal">
+                      Ekun
+                    </p>
+                  </div>
+
+                  <div className="w-fit flex flex-col">
+                    <h4 className="font-inter font-medium text-base tracking-[0.06em]">
+                      Academic year
+                    </h4>
+                    <p className="tracking-[0.06em] text-sm font-montserrat font-normal">
+                      Unknown
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2 className="font-semibold text-2xl font-inter tracking-[0.06em] text-primary">
+                Promotion history
+              </h2>
+            </div>
+          </div>
         </div>
       </DashboardLayout>
     </>
