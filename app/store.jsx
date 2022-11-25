a4da5182ed4425a299260349051d65ec674ea5ab
Senninseyi <seyi.oyebamiji@gmail.com>
2022-11-25 06:34:31 +0100

    chore: create a redux slice for selecting payment

--- app/store.jsx
+++ app/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import DashboardReducer from "./slices/dashboardSlice";
+import PaymentReducer from "./slices/paymentSlice";
 
 const store = configureStore({
   reducer: {
     dashboard: DashboardReducer,
+    payment: PaymentReducer,
   },
 });
 
