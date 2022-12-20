9f2214f182c74153dfc26ee94d74c147b0f7964d
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-21 00:10:05 +0100

    code refractoring

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import React from "react";
 import DashboardLayout from "../components/layout/dashboardLayout";
 import { dashboardLinks, bottomdashboardLinks } from "../data/data";
-import LoadingScreen from "../components/loaderscreen/loadingscreen";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
