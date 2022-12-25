3b334d68ef9deac7a400c622c93eeef0fd7cf32e
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-25 05:34:18 +0100

    chore: add authreducer to redux-persist

--- app/store.jsx
+++ app/store.jsx
@@ -1,14 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import DashboardReducer from "./slices/dashboardSlice";
 import PaymentReducer from "./slices/paymentSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import AuthReducer from "./slices/authSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, AuthReducer);
 
 const store = configureStore({
   reducer: {
     dashboard: DashboardReducer,
     payment: PaymentReducer,
-    auth: AuthReducer,
+    auth: persistedReducer,
   },
 });
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor };
